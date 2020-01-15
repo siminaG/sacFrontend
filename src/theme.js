227b526b133fd705c0b0d7b021dd295a4effee83
Anamaria Lungeanu <anamarialungeanu@gmail.com>
2020-01-15 23:26:20 +0200

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#19857b',
     },
     secondary: {
       main: '#19857b',
@@ -14,7 +14,7 @@ const theme = createMuiTheme({
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: '#C5FFD0',
     },
   },
 });
